352c47381fa9b141d1279d7cb8b3074b87f84675
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-08 16:45:05 +0100

    fix: fixed the hamburger menu for mobile

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,9 +1,23 @@
 const navLinks = document.getElementById('navLinks')
 const btn = document.getElementById('btn')
-const hamburgerMenu = document.getElementById('hamburger_menu')
+const hamburgerOpenMenu = document.getElementById('hamburger_openMenu')
+const hamburgerCloseMenu = document.getElementById('hamburger_closeMenu')
 
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('close')
-  // navLinks.style.display = 'flex'
-  // btn.style.display = 'block'
-})
+hamburgerOpenMenu.addEventListener('click', show)
+hamburgerCloseMenu.addEventListener('click', hide)
+
+function show() {
+  navLinks.style.display = 'flex'
+  navLinks.style.top = '3.7rem'
+  btn.style.display = 'block'
+  btn.style.top = '60%'
+  hamburgerCloseMenu.style.display = 'block'
+  hamburgerOpenMenu.style.display = 'none'
+}
+
+function hide() {
+  navLinks.style.top = '-100%'
+  btn.style.top = '-100%'
+  hamburgerCloseMenu.style.display = 'none'
+  hamburgerOpenMenu.style.display = 'block'
+}
