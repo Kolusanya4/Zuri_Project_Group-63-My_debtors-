5ed794cca4d4f2d2930acb138c85334fe240f7c1
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-12-09 16:22:43 +0100

    models.py file in app worked by Emmanuel Solomon

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def Homepage(request):
 <<<<<<< HEAD
-    return render(request,'footer.html')
+    return render(request,'home.html')
 =======
     return render(request,'home.html')
 >>>>>>> f013b8f0948166a06d140ff893b934772cb587d1
