86054de4923ec9596cca5a53ae5b086d1adb7bcd
Neon-jeff <omaksteejay@gmail.com>
2022-12-14 22:26:58 +0100

    Configured Whitenoise

--- mydebtors/settings.py
+++ mydebtors/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'accounts',
     'app'
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,11 +122,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR=os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'static/'),os.path.join(BASE_DIR,'static/images'),
-   os.path.join(BASE_DIR,'static/css'),os.path.join(BASE_DIR,'static/scripts')
+   os.path.join(BASE_DIR,'static/')
 
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
