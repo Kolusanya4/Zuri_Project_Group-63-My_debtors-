691f8eafc45fe22c2c566a161879f071f5986df6
Neon-jeff <omaksteejay@gmail.com>
2022-12-14 21:24:53 +0100

    Fixed staticfiles

--- mydebtors/settings.py
+++ mydebtors/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR=os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR,'static/')
+   os.path.join(BASE_DIR,'static/'),os.path.join(BASE_DIR,'static/images'),
+   os.path.join(BASE_DIR,'static/css'),os.path.join(BASE_DIR,'static/scripts')
+
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
